56f4b766bea6894ae69d287f1603c6610b0f06be
k <kebai0624@gmail.com>
2017-06-11 16:41:23 +0000

    add read me

---
+++ readme.md
@@ -0,0 +1,3 @@
+![image](https://github.com/beckybai/super_market/image/User_process.png.odg)
+
+![image](https://github.com/beckybai/super_market/image/Admin_process.png.odg)
--- test.cpp
+++ test.cpp
@@ -134,7 +134,7 @@ vector<Administrator_Info*> get_administrator_info(){
 //	    fp = fopen("in.txt", "r");//打开文件
 	    while(fgets(tmp_line, sizeof(tmp_line), fp))//每次读取一行数据，直到读取失败。
 	    {
-	        printf("%s\n", tmp_line); //打印该行。
+	        // printf("%s\n", tmp_line); //打印该行。
 	        // The structure of this is username, password, money, items_buy.
 	        Administrator_Info* new_admin = new Administrator_Info();
 	        char* name = strtok( tmp_line, ",");
@@ -163,7 +163,7 @@ vector<Customer_Info*> get_customer_info(){
 //	    fp = fopen("in.txt", "r");//打开文件
 	    while(fgets(tmp_line, sizeof(tmp_line), fp))//每次读取一行数据，直到读取失败。
 	    {
-	        printf("%s\n", tmp_line); //打印该行。// TO DO: cancel this!!
+	        // printf("%s\n", tmp_line); //打印该行。// TO DO: cancel this!!
 	        // The structure of this is username, password, money, items_buy.
 	        Customer_Info* new_customer = new Customer_Info();
 	        char* name = strtok( tmp_line, ",");
@@ -266,7 +266,7 @@ vector<Good_Info*> get_goods_info(){
 //	    fp = fopen("in.txt", "r");//打开文件
 	    while(fgets(tmp_line, sizeof(tmp_line), fp))//每次读取一行数据，直到读取失败。
 	    {
-	        printf("%s\n", tmp_line); //打印该行。
+	        // printf("%s\n", tmp_line); //打印该行。
 	        // The structure of this is username, password, money, items_buy.
 	        Good_Info* good = new Good_Info();
 	        // char* delim = ",";
@@ -354,6 +354,25 @@ void write_goods_info(vector<Good_Info*> vgi){
 	}
 }
 
+void delete_good(vector<Good_Info*> good_list,int tmp_delete){
+		int flag =  0;
+		int good_list_size = good_list.size();
+		int tmp_i = 0;
+		for(tmp_i=0;tmp_i<good_list_size;tmp_i++){
+			if(good_list[tmp_i]->ID==tmp_delete){
+				good_list.erase(good_list.begin()+tmp_i);
+				flag=1;
+			}
+		}
+		if(flag){
+			printf("You have successfully delete the object!\n");
+		}
+		else{
+			printf("Can't find the object you want to delete!\n");
+		}
+}
+
+
 
 void write_cart(string name, int ID, string good_name, int num ){
 	FILE *fp;
@@ -401,11 +420,60 @@ vector<Shopping_Cart_Info*> get_cart_info(){
 	}
 }
 
+vector<Shopping_Cart_Info*> get_buy_info(){
+	FILE* fp;
+	vector<Shopping_Cart_Info*> sci;
+	if(NULL==(fp=fopen("buy_info.txt","r"))){
+		printf("error when opening the buy_info.txt");
+		exit(-1);
+	}
+	else{
+		char tmp_line[100];
+//	    fp = fopen("in.txt", "r");//打开文件
+	    while(fgets(tmp_line, sizeof(tmp_line), fp))//每次读取一行数据，直到读取失败。
+	    {
+	        // printf("%s\n", tmp_line); //打印该行。
+	        // The structure of this is username, password, money, items_buy.
+	        Shopping_Cart_Info* cart = new Shopping_Cart_Info();
+	        // char *delim = ",";
+	        char* name = strtok(tmp_line, ",");
+	        cart->name = name;
+	        char* id = strtok(NULL, ",");
+	        cart->ID = atoi(id);
+//	        good->place_origin = place_origin;
+	        char* good_name = strtok(NULL,",");
+	        cart->good_name = good_name;
+	        char* num = strtok(NULL,",");
+	        cart->num = atoi(num);
+	        sci.push_back(cart);
+	    }
+	    fclose(fp);
+	    return sci;
+	}
+}
+
 
 void write_cart_info(vector<Shopping_Cart_Info*> sci){
 	FILE* fp;
 	if(NULL==(fp=fopen("cart_info.txt","w"))){
 		printf("error when opening the cart_info.txt");
+		exit(-1);
+	}
+	else{
+		int sci_len = sci.size();
+		int tmp_i = 0;
+		for(tmp_i;tmp_i<sci_len;tmp_i++){
+			Shopping_Cart_Info* cart = sci[tmp_i];
+			fprintf(fp,"%s,%d,%s,%d\n",(cart->name).c_str(),cart->ID,(cart->good_name).c_str(),cart->num);
+		}
+	}
+	fclose(fp);
+}
+
+void write_buy_info(vector<Shopping_Cart_Info*> sci){
+	FILE* fp;
+	if(NULL==(fp=fopen("buy_info.txt","w"))){
+		printf("error when opening the buy_info.txt");
 		exit(-1);
 	}
 	else{
@@ -445,27 +513,19 @@ void scan_good_store(){
 				printf("%d\t",tmp_i);
 				print_good_store(good_list[tmp_i]);
 			}
+
+			for(tmp_i=0;tmp_i<good_list_size;tmp_i++){
+				if(good_list[tmp_i]->save_amount <10){
+					printf("This number of %s is not enough !\n", good_list[tmp_i]->name);
+				}
+			}
 			break;
 		}
 		case 2:{
 			printf("Please input the id of the object you want to delete!\n");
 			int tmp_delete;
 			scanf("%d",&tmp_delete);
-			int flag =  0;
-			int good_list_size = good_list.size();
-
-			for(tmp_i=0;tmp_i<good_list_size;tmp_i++){
-				if(good_list[tmp_i]->ID==tmp_delete){
-					good_list.erase(good_list.begin()+tmp_i);
-					flag=1;
-				}
-			}
-			if(flag){
-				printf("You have successfully delete the object!\n");
-			}
-			else{
-				printf("Can't find the object you want to delete!\n");
-			}
+			delete_good(good_list,tmp_delete);
 			break;
 		}
 		case 3:{
@@ -490,6 +550,7 @@ void scan_good_store(){
 			new_one->place_origin = get_string();
 			printf("Input the store where it exists\n");
 			new_one->place_now = get_string();
+			new_one->place_now +='\n';
 			printf("Input the total amount\n");
 			int tmp_amount;scanf("%d",&tmp_amount);fflush(stdin);
 			new_one->save_amount = tmp_amount;
@@ -583,9 +644,9 @@ void scan_good(Customer_Info* customer){
 	int good_list_size = good_list.size();
 	int tmp_i = 0;
 
-	printf("---------------------------------------------------------\n");
+	printf("------------------------Goods_Info---------------------------------\n");
 	printf("No\tID\tPrice\tName\tOrigin_place\tSave_amount\tOut_amount\tPlace_now\n");
-	printf("---------------------------------------------------------\n");
+	printf("-------------------------------------------------------------------\n");
 
 
 	for(tmp_i;tmp_i<good_list_size;tmp_i++){
@@ -646,23 +707,77 @@ void print_shopping_cart(Customer_Info*customer){
 	vector<Shopping_Cart_Info*> sci = get_cart_info();
 	int sci_len = sci.size();
 	int tmp_i = 0;
+	printf("-----------------------Your Shopping_Cart_Info----------------------------\n");
 	printf("ID\tname\tnumber\n");
+	printf("--------------------------------------------------------------------------\n");
+
 	for(tmp_i;tmp_i<sci_len;tmp_i++){
-		if(sci[tmp_i]->name==customer->username)
+		if(sci[tmp_i]->name==customer->username && sci[tmp_i]->num!=0)
 			printf("%d\t%s\t%d\n",sci[tmp_i]->ID,(sci[tmp_i]->good_name).c_str(),sci[tmp_i]->num);
 	}
 }
 
+void print_buy_info(Customer_Info *customer){
+	vector<Shopping_Cart_Info*> sci = get_buy_info();
+	int sci_len = sci.size();
+	int tmp_i = 0;
+	printf("-----------------------Things you already buy-----------------------------\n");
+	printf("ID\tname\tnumber\n");
+	printf("--------------------------------------------------------------------------\n");
+
+	for(tmp_i;tmp_i<sci_len;tmp_i++){
+		if(sci[tmp_i]->name==customer->username && sci[tmp_i]->num!=0)
+			printf("%d\t%s\t%d\n",sci[tmp_i]->ID,(sci[tmp_i]->good_name).c_str(),sci[tmp_i]->num);
+	}
+}
+
+void return_goods(Customer_Info * customer){
+	vector<Shopping_Cart_Info*> sci = get_buy_info();
+	vector<Good_Info*> good_list = get_goods_info();
+	int good_list_size = good_list.size();
+	print_buy_info(customer);
+	printf("Print the ID of the object you want to give it back?\n");
+	int tmp_back;
+	int tmp_back_num;
+	scanf("%d",&tmp_back);
+	int flag =  0;
+	int tmp_i = 0;
+	for(tmp_i=0;tmp_i<good_list_size;tmp_i++){
+		if(good_list[tmp_i]->ID==tmp_back){
+			// good_list.erase(good_list.begin()+tmp_i);
+			flag=1;
+			printf("Print the number of the object you want to give it back. Please do not over the things you already buy \n");
+			scanf("%d",&tmp_back_num);
+			break;
+		}
+	}
+	if(flag){
+		good_list[tmp_i]->save_amount += tmp_back_num;
+		customer->money += good_list[tmp_i]->price * tmp_back_num;
+		printf("Now, your pocket has %f Yuan\n", customer->money);
+		sci.erase(sci.begin()+tmp_i);
+		write_goods_info(good_list);
+		write_buy_info(sci);
+	}
+	else{
+		printf("You may input the wrong info.\n");
+	}
+
+}
+
 void clear_cart(Customer_Info*customer){
 	//1st: find the user's record in the shopping cart file;
-	vector<Shopping_Cart_Info*> sci = get_cart_info();
+	vector<Shopping_Cart_Info*> sci = get_cart_info(); // THings in the shoppoing cart
+	vector<Shopping_Cart_Info*> buy_good = get_buy_info();
 	printf("Be sure of the safe1\n");
-	vector<Good_Info*> gi  = get_goods_info();
+	vector<Good_Info*> gi  = get_goods_info(); // Things in the store
 
-	vector<Customer_Info*> ci = get_customer_info();
+	// vector<Customer_Info*> ci = get_customer_info(); // Get the information of all the customers.
 		printf("Be sure of the safe2\n");
 	int sci_len = sci.size();
 	int gi_len = gi.size();
+	printf("shopping cart size: %d\n", sci_len);
+
 	int tmp_i = 0;
 	string username = customer->username;
 
@@ -672,8 +787,11 @@ void clear_cart(Customer_Info*customer){
 	{
 		int tmp_j = 0;
 		Shopping_Cart_Info* tmp_sci = sci[tmp_i];
+		Shopping_Cart_Info* tmp_buy = new Shopping_Cart_Info();
+
 		int ID = tmp_sci->ID;
 		string name = tmp_sci->name;
+		int num_buy = tmp_sci ->num;
 		if(username==name){
 			for(tmp_j;tmp_j<gi_len;tmp_j++){
 				if(ID == gi[tmp_j]->ID){
@@ -681,11 +799,19 @@ void clear_cart(Customer_Info*customer){
 					if(gi[tmp_j]->save_amount >= tmp_sci->num){	//2ed: check if there are enough things in the storage.
 						gi[tmp_j]->save_amount -=tmp_sci->num;
 						total_expense += gi[tmp_j]->price * tmp_sci->num;
+						sci[tmp_i]->num = 0;
 						if(total_expense > customer->money){
-							printf("Sorry, you can only buy these things because you don't have enough money!");
+							printf("Sorry, you can only buy %s because you don't have enough money!", sci[tmp_i]->name);
+							continue;
 						}
-						else
-							sci.erase(sci.begin()+tmp_i); // Totally removed from the shoppint cart.
+						else{
+							customer->money = customer->money - total_expense;
+							tmp_buy->ID = ID;
+							tmp_buy->good_name = gi[tmp_j]->name;
+							tmp_buy->name = name;
+							tmp_buy->num = num_buy;
+							buy_good.push_back(tmp_buy);
+						}
 						// TO DO  Change the information of the customer.
 					}
 					else if(gi[tmp_j]->save_amount>0){
@@ -697,6 +823,12 @@ void clear_cart(Customer_Info*customer){
 						else{
 							sci[tmp_i]->num -= gi[tmp_j]->save_amount;
 							gi[tmp_j]->save_amount = 0;
+							customer->money = customer->money - total_expense;
+							tmp_buy->ID = ID;
+							tmp_buy->good_name = gi[tmp_j]->name;
+							tmp_buy->name = name; 
+							tmp_buy->num = gi[tmp_j]->save_amount;
+							buy_good.push_back(tmp_buy);
 						}
 					}
 					else{
@@ -706,9 +838,39 @@ void clear_cart(Customer_Info*customer){
 			}
 		}
 	}
+	printf("-----------------------------------------------------------\n");
+	printf("Attention!\n You spend %d Yuan in total\n",total_expense);
+	printf("You still have %.2f Yuan in yoru account\n", customer->money);
+	printf("-----------------------------------------------------------\n");
+
+
+	vector<Shopping_Cart_Info*>::iterator it = sci.begin();
+	// printf("%d",(**it).num);
+	// printf("233\n"); 
+	tmp_i = 0;
+	for(it;it!=sci.end();)
+    {
+    	// printf("num: %d",(*it)->num);
+        if(sci[tmp_i++]->num==0){ // Delete the things in the cart which the customer has already bought.
+        	// printf("Safe Alert!\n");
+        	// ++it;
+            it=sci.erase(it);    //删除元素，返回值指向已删除元素的下一个位置    
+        }
+        else
+            ++it;    //指向下一个位置
+    }
+	// for(tmp_i=0;tmp_i<sci_len;tmp_i++){
+	// 	if(sci[tmp_i]->num==0){
+	// 		sci.erase(sci.begin()+tmp_i);
+	// 		// sci_len--;
+	// 		// tmp_i--;
+	// 	}
+	// }
+
 	//3rd: update the storage. update the shopping cart.
 	write_goods_info(gi);
 	write_cart_info(sci);
+	write_buy_info(buy_good);
 	//TO DO  Update the information of the customer.
 }
 
@@ -822,11 +984,11 @@ void Customer(){
 				printf("4. Clear your Cart, Buy\n");
 				printf("5. Send back the good\n");
 				printf("-1 exit\n");
-				char a = 'a';
+				// char a = 'a';
 				int tmp_char = get_num();
 				switch(tmp_char){
 					case 1:
-//						print_own_things();
+						print_buy_info(this_one);
 						break;
 					case 2:
 						scan_good(this_one);
@@ -838,18 +1000,16 @@ void Customer(){
 						clear_cart(this_one);
 						break;
 					case 5:
-//						return_goods();
+						return_goods(this_one);
 						break;
 
-					case 0:
+					case -1:
+						write_customer_info(customer_list);
 						printf("Thanks for your log in, Good bye!\n");
 						return;
 					default:
-						printf("choose an operation to consist on or press q to exit\n");
+						printf("PLease input a right number\n");
 						// char tmp ;
-						scanf("%c",&a);fflush(stdin);
-						if(a=='q')
-							break;
 						break;
 				}
 			}
@@ -952,12 +1112,12 @@ void Adminstrator_R(){
 
 
 int main(void) {
-	printf("Hello World"); /* prints Hello World */
+	// printf("Hello World");  prints Hello World 
 	int identity=0; //
 
 	while(1){
 		if(identity==0){
-			printf("Welcome to JD system! Login as 1. Administrator 2. Customer(Default) 3. Customer Register 4. Administrator Register q. Exit\n");
+			printf("Welcome to JD system! Login as 1. Administrator 2. Customer(Default) 3. Customer Register 4. Administrator Register -1 Exit\n");
 			int tmp ;
 			scanf("%d",&tmp);
 			fflush(stdin);
@@ -978,7 +1138,7 @@ int main(void) {
 					case 4:
 						Adminstrator_R();
 						break;
-					case 0:
+					case -1:
 						identity = 1;
 						break;
 //						return 0;
@@ -995,7 +1155,7 @@ int main(void) {
 		else{
 			printf("Do you want to Login a new Account? 1. Yes 2. No(Default)\n");
 			int tmp;
-			printf("waht??\n");
+			// printf("waht??\n");
 			scanf("%d",&tmp);
 			fflush(stdin);
 			if(tmp=='\r' || tmp==2 || tmp=='\n'){
